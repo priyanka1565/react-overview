040c6223204badd4dad7313c8aaa5f043a54b98f
priyanka1565 <priyankaingle250@gmail.com>
2023-03-28 20:27:37 +0530

    added

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Hooks from './component/Hooks';
+import HookCounter from './component/HookCounter';
 
 function App() {
   return (
     <div>
-      <Hooks/>
+      {/* <Hooks/> */}
+      <HookCounter/>
     </div>
   );
 }
---
+++ my-app/src/component/HookCounter.jsx
@@ -0,0 +1,19 @@
+import React, { useState,useEffect } from 'react'
+
+const HookCounter = () => {
+
+    const [count, setCount] = useState(0)
+    const [ name, setName ] = useState("")
+    useEffect(() => {
+        document.title = `You clicked ${count} times`
+    })
+
+  return (
+      <div>
+          <input type="text" value={name} onChange={e=>setName( e.target.value)} />
+          <button onClick={() => setCount(count + 1)}>Click { count} times</button>
+    </div>
+  )
+}
+
+export default HookCounter
