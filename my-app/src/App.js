a678e260c33ddaa6a55797a5c02e4470617cd7f8
priyanka1565 <priyankaingle250@gmail.com>
2023-03-28 19:39:30 +0530

    useEffect example

---
+++ my-app/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Hooks from './component/Hooks';
+
+function App() {
+  return (
+    <div>
+      <Hooks/>
+    </div>
+  );
+}
+
+export default App;
